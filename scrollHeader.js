59f5540d2ffe1a7742b13f7934cf9920922b418c
nuco-san <81429191+nuco-san@users.noreply.github.com>
2022-04-13 10:00:05 +0200

    cambio font completato

--- scrollHeader.js
+++ scrollHeader.js
@@ -3,11 +3,13 @@
 		setInitialValues();
 
 		window.addEventListener('resize', function(event){
+			maxSize = document.getElementById("head").offsetHeight + 50;
 			setInitialValues();
 		});
 
 		function setInitialValues() {
 			if(window.innerWidth > 650) {
+				maxSize = document.getElementById("head").offsetHeight + 50;
 				document.getElementById("contenitore-immagini").style.marginTop = maxSize + "px";
 				document.getElementById("head").style.height = maxSize + "px";
 			}
