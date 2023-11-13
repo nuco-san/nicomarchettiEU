e3f482b3b6afb6f2c55ebdb7a5557eba9899b7ba
nuco <81429191+nuco-san@users.noreply.github.com>
2023-11-13 16:20:56 +0100

    implementato versione mobile della home

--- mainMenu.js
+++ mainMenu.js
@@ -3,18 +3,14 @@ var menuActivated = false;
 function openCloseMenu() {
     if(menuActivated == false){
         menuActivated = true;
-        document.getElementById("icona-home").src = "/Burger_2.png";
-        document.getElementById("main-menu").style.display = "block";
-        document.getElementById("main-menu-container").classList.add("menu-shown");
-        document.getElementById("main-menu-container").classList.remove("menu-hidden");
+        document.getElementById("mobile-main-menu").classList.add("menu-shown");
+        document.getElementById("mobile-main-menu").classList.remove("menu-hidden");
         document.body.style.overflow= "hidden";
     }
     else {
         menuActivated = false;
-        document.getElementById("icona-home").src = "/Burger_1.png";
-        document.getElementById("main-menu").style.display = "none";
-        document.getElementById("main-menu-container").classList.add("menu-hidden");
-        document.getElementById("main-menu-container").classList.remove("menu-shown");
+        document.getElementById("mobile-main-menu").classList.remove("menu-shown");
+        document.getElementById("mobile-main-menu").classList.add("menu-hidden");
         document.body.style.overflow= "auto";
     }
 }
