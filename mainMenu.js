20b0f001f5982216a01abfd07e5f81bb860ab5d0
nuco <81429191+nuco-san@users.noreply.github.com>
2023-11-13 16:25:04 +0100

    piccoli bug

--- mainMenu.js
+++ mainMenu.js
@@ -3,6 +3,7 @@ var menuActivated = false;
 function openCloseMenu() {
     if(menuActivated == false){
         menuActivated = true;
+        document.getElementById("mobile-main-menu").style.display = "block";
         document.getElementById("mobile-main-menu").classList.add("menu-shown");
         document.getElementById("mobile-main-menu").classList.remove("menu-hidden");
         document.body.style.overflow= "hidden";
