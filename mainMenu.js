74211c41c30f7e5187f26e3ef96da41e46969ef1
nuco <81429191+nuco-san@users.noreply.github.com>
2023-11-18 11:37:23 +0100

    aggiustato menu in project

--- mainMenu.js
+++ mainMenu.js
@@ -6,12 +6,10 @@ function openCloseMenu() {
         document.getElementById("mobile-main-menu").style.display = "block";
         document.getElementById("mobile-main-menu").classList.add("menu-shown");
         document.getElementById("mobile-main-menu").classList.remove("menu-hidden");
-        document.body.style.overflow= "hidden";
     }
     else {
         menuActivated = false;
         document.getElementById("mobile-main-menu").classList.remove("menu-shown");
         document.getElementById("mobile-main-menu").classList.add("menu-hidden");
-        document.body.style.overflow= "auto";
     }
 }
